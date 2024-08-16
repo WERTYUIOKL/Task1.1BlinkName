755a1dfc97e7fc6890cc788a67ae47113d573437
WERTYUIOKL <147541259+WERTYUIOKL@users.noreply.github.com>
2024-08-16 11:06:55 +0530

    Add files via upload

---
+++ AkshitMorseCode.ino
@@ -0,0 +1,95 @@
+#include "thingProperties.h"
+
+const int ledPin = LED_BUILTIN;
+
+const int dotDuration = 200;
+const int dashDuration = 3 * dotDuration;
+const int elementSpace = dotDuration;
+const int letterSpace = 3 * dotDuration;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+
+  delay(1500);
+
+  initProperties();
+
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+  
+  setDebugMessageLevel(2);
+  ArduinoCloud.printDebugInfo();
+}
+
+void loop() {
+  ArduinoCloud.update();
+
+  Serial.print("LED Value: ");
+  Serial.println(led);
+
+  if (led) {
+    blinkA();
+    blinkK();
+    blinkS();
+    blinkH();
+    blinkI();
+    blinkT();
+  }
+}
+
+void onLedChange() {
+  Serial.println("LED state changed from IoT Cloud");
+}
+
+void blinkDot() {
+  digitalWrite(ledPin, HIGH);
+  delay(dotDuration);
+  digitalWrite(ledPin, LOW);
+  delay(elementSpace);
+}
+
+void blinkDash() {
+  digitalWrite(ledPin, HIGH);
+  delay(dashDuration);
+  digitalWrite(ledPin, LOW);
+  delay(elementSpace);
+}
+
+void blinkA() {
+  blinkDot();
+  blinkDash();
+  delay(letterSpace);
+}
+
+void blinkK() {
+  blinkDash();
+  blinkDot();
+  blinkDash();
+  delay(letterSpace);
+}
+
+void blinkS() {
+  blinkDot();
+  blinkDot();
+  blinkDot();
+  delay(letterSpace);
+}
+
+void blinkH() {
+  blinkDot();
+  blinkDot();
+  blinkDot();
+  blinkDot();
+  delay(letterSpace);
+}
+
+void blinkI() {
+  blinkDot();
+  blinkDot();
+  delay(letterSpace);
+}
+
+void blinkT() {
+  blinkDash();
+  delay(letterSpace);
+}
